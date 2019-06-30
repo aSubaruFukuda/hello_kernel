6bc7ece1d11ada4a97f0169e23fa6e584532f1bb
Suabru Fukuda <subaru.fukuda@gmail.com>
2019-06-30 10:44:02 +0900

    specify whom on cmdline option

--- hello.c
+++ hello.c
@@ -5,13 +5,16 @@ MODULE_DESCRIPTION("HELLO KERNEL");
 MODULE_AUTHOR("subaruf");
 MODULE_LICENSE("GPL");
 
+static char *whom = "name";
+module_param(whom, charp, S_IRUGO);
+
 static int hello(void) {
-	printk("hello kernel\n");
+	printk(KERN_ALERT "hello %s\n", whom);
 	return 0;
 }
 
 static void bye(void) {
-	printk("bye kernel\n");
+	printk(KERN_ALERT "bye %s\n", whom);
 }
 
 module_init(hello);
